b57dadf4505a2bd099ac87b800fd411b0d427747
AlexAxenti <alex.axenti@gmail.com>
2025-12-21 22:38:12 -0500

    initial exercises folder

---
+++ exercises/hello-world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\CodeMaster\Desktop\RustLearning\exercises\hello-world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\CodeMaster\Desktop\RustLearning\exercises\hello-world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
---
+++ exercises/hello-world/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Users\CodeMaster\Desktop\RustLearning\exercises\hello-world\target\debug\hello-world.exe: C:\Users\CodeMaster\Desktop\RustLearning\exercises\hello-world\src\main.rs
