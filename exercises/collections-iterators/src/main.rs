51fef98385be370dcbc9aa2d65961e3c5ffd80ea
AlexAxenti <alex.axenti@gmail.com>
2025-12-23 00:21:36 -0500

    more iterations

--- exercises/collections-iterators/src/main.rs
+++ exercises/collections-iterators/src/main.rs
@@ -1,14 +1,27 @@
-fn sum(nums: &[i32]) -> i32 {
-    nums.iter().copied().sum()
+// fn sum(nums: &[i32]) -> i32 {
+//     nums.iter().copied().sum()
+// }
+
+// fn max(nums: &[i32]) -> Option<i32> {
+//     nums.iter().copied().max()
+// }
+
+// fn contains(nums: &[i32], target: i32) -> bool {
+//     nums.iter().any(|n| *n == target)
+// }
+
+fn analyze(nums: &[i32]) -> (i32, Option<i32>, usize, Vec<i32>) {
+    let sum = nums.iter().copied().sum();
+
+    let max = nums.iter().copied().max();
+
+    let count_nums_odd: usize = nums.iter().copied().filter(|n| *n % 2 != 0).count();
+
+    let new_nums: Vec<i32> = nums.iter().copied().filter(|n| *n > 0 && *n % 3 == 0).map(|n| n * n).collect();
+
+    (sum, max, count_nums_odd, new_nums)
 }
 
-fn max(nums: &[i32]) -> Option<i32> {
-    nums.iter().copied().max()
-}
-
-fn contains(nums: &[i32], target: i32) -> bool {
-    nums.iter().any(|n| *n == target)
-}
 
 fn main() {
     let nums: Vec<i32> = vec![10, 20, 30, 40];
@@ -42,4 +55,9 @@ fn main() {
         Some(n) => println!("found: {}", n),
         None => println!("no value at that index"),
     }
+
+    let nums = vec![-3, -1, 0, 1, 3, 6, 7, 9, 10];
+
+    let result = analyze(&nums);
+    println!("{:?}", result);
 }
