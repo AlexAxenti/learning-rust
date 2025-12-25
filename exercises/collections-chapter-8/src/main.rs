3993d42a1ba91c84416a1dff62a6834c4ffc5651
AlexAxenti <alex.axenti@gmail.com>
2025-12-25 00:21:13 -0500

    Median and Mode

--- exercises/collections-chapter-8/src/main.rs
+++ exercises/collections-chapter-8/src/main.rs
@@ -1,3 +1,47 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut numbers = vec![5, 2, -4, 8, 1, 5, 2, 2];
+
+    numbers.sort();
+
+    println!("Sorted vector (asecnding): {:?}", numbers);
+
+    println!("median: {}", find_median(&numbers));
+
+    println!("mode: {}", find_mode(&numbers));
+}
+
+
+fn find_median(list: &[i32]) -> i32 {
+    let length = list.len();
+
+    let median_index = length / 2;
+
+    println!("median index is {median_index}");
+
+    list[median_index]
+}
+
+fn find_mode(list: &[i32]) -> i32 {
+    let mut mode_map = HashMap::new();
+
+    for n in list {
+        let count = mode_map.entry(*n).or_insert(0);
+        *count += 1;
+    }
+
+    println!("Mode Hash map: {mode_map:?}");
+
+    let mut max_occurances: usize = 0;
+    let mut max_key: i32 = 0;
+
+    for (key, value) in &mode_map {
+        if mode_map.get(key).copied().unwrap_or(0) > max_occurances { 
+            max_occurances = *value;
+            max_key = *key;
+        }
+    }
+
+    max_key
 }
