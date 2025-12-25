d6c5502bdb4783e6c6c2963b017d0e65e5d3a075
AlexAxenti <alex.axenti@gmail.com>
2025-12-25 11:48:37 -0500

    Chapter 8 exercises completed

--- exercises/collections-chapter-8/src/main.rs
+++ exercises/collections-chapter-8/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io};
 
 fn main() {
     let mut numbers = vec![5, 2, -4, 8, 1, 5, 2, 2];
@@ -14,9 +14,99 @@ fn main() {
     let english = "Hey how are you doing";
 
     println!("{english} converted to pig latin: {}", convert_to_pig_latin(english));
+
+    manage_company();
 }
 
+// Exercise 3
+fn manage_company() {
+    let mut company_directory: HashMap<String, Vec<String>>  = HashMap::new();
 
+    println!("Welcome to managing your company!");
+    loop {
+        let mut input_text = String::new();
+        display_options();
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("Failed to read line");
+
+        match input_text.trim() {
+            "1" => {
+                print_existing_companies(&company_directory);
+            }
+            "2" => {
+                println!("{company_directory:#?}");
+            }
+            "3" => {
+                add_new_department(&mut company_directory);
+            }
+            "4" => {
+                add_new_employee(&mut company_directory);
+            }
+            "5" => {
+                break;
+            }
+            _ => {
+                println!("Invalid input");
+                continue;
+            }
+        }
+    }
+}
+
+fn display_options() {
+    println!("Select a control:");
+    println!("(1): View departments");
+    println!("(2): View company");
+    println!("(3): Create department");
+    println!("(4): Add employee");
+    println!("(5): Exit");
+}
+
+fn print_existing_companies(company_directory: &HashMap<String, Vec<String>>) {
+    println!("Existing departments:");
+    for (key, _) in company_directory {
+        println!("{key}");
+    }
+}
+
+fn add_new_department(company_directory: &mut HashMap<String, Vec<String>>) {
+    let mut new_department = String::new();
+    println!("Input department name:");
+    io::stdin()
+        .read_line(&mut new_department)
+        .expect("Failed to read line");
+
+    let new_department = new_department.trim().to_string();
+    company_directory.entry(new_department).or_insert(vec![]);
+}
+
+fn add_new_employee(company_directory: &mut HashMap<String, Vec<String>>) {
+    let mut department = String::new();
+    let mut new_employee = String::new();
+
+    println!("Enter new employee's name:");
+
+    io::stdin()
+        .read_line(&mut new_employee)
+        .expect("Failed to read line");
+
+    let new_employee = new_employee.trim().to_string();
+
+
+    println!("Enter new or existing department:");
+
+    io::stdin()
+        .read_line(&mut department)
+        .expect("Failed to read line");
+
+    let department = department.trim().to_string();
+    let found_directory = company_directory.entry(department).or_insert(vec![]);
+
+    found_directory.push(new_employee);
+}
+
+// Exercise 1
 fn find_median(list: &[i32]) -> i32 {
     let length = list.len();
 
@@ -50,6 +140,7 @@ fn find_mode(list: &[i32]) -> i32 {
     max_key
 }
 
+// Exercise 2
 fn convert_to_pig_latin(s: &str) -> String {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let mut pig_latin_s = String::new();
